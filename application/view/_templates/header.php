2e30223e86a2084fe7d8fc8bbc4299f9f450f9c4
Brandon Glajchen <glajchen@mail.sfsu.edu>
2016-07-11 22:46:15 +0000

    Fixed title from "SFSU Bazzar" to "SFSU Bazaar"

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>SFSU Bazzar</title>
+    <title>SFSU Bazaar</title>
     <meta charset="utf-8">
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
