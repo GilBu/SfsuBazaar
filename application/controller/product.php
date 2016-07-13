da41031124e7b967540a06bb1f314187954e2c91
Guan Lin <glin@mail.sfsu.edu>
2016-07-12 17:11:59 -0700

    added add(), save(), getData(), processUploadedImage(), and documentation

--- application/controller/product.php
+++ application/controller/product.php
@@ -6,40 +6,84 @@
 
 class Product extends Controller
 {   
+    /**
+     * PAGE: index
+     * Renders the product's index page
+     */
     public function index() 
     {
         
     }
     
-    public function newProduct()
+    /**
+     * PAGE: add
+     * Renders the product's add page
+     */
+    public function add()
     {   
         require APP . 'view/_templates/header.php';
-        require APP . 'view/product/newProduct.php';
+        require APP . 'view/product/add.php';
         require APP . 'view/_templates/footer.php';
     }
     
-    public function add()
+    /**
+     * ACTION: save
+     * Saves the product infomation to the database
+     */
+    public function save()
     {
-        $product = array();
-        
+        // check if the form is set
         if (isset($_POST['addProduct']))
         {
-            $product['name'] = filter_input(INPUT_POST, 'name');
-            $product['price'] = filter_input(INPUT_POST, 'price');
-            $product['seller_id'] = filter_input(INPUT_POST, 'seller_id');
-            $product['picture'] = filter_input(INPUT_POST, 'picture');
-            $product['video'] = filter_input(INPUT_POST, 'video');
-            $product['description'] = filter_input(INPUT_POST, 'description');
-            $product['hourly'] = filter_input(INPUT_POST, 'hourly');
+            // get all data about the product
+            $product = $this->getData();
             
-            if ($product['hourly'] == "") 
-            {
-                $product['hourly'] = "0";
-            }
-
+            // save product to database
             $this->products->add($product);
         }
         
+        // redirect to listing/index page
         header('location: ' . URL . 'listing/index');
+    }
+    
+    /**
+     * ACTION HELPER: getData
+     * Get all the product's data
+     * @return Associative Array with all data about the product
+     */
+    private function getData()
+    {   
+        $product = array();
+        
+        $product['name'] = filter_input(INPUT_POST, 'name');
+        $product['price'] = filter_input(INPUT_POST, 'price');
+        $product['seller_id'] = filter_input(INPUT_POST, 'seller_id');
+        $product['picture'] = $this->processUploadedImage('picture');
+        $product['video'] = filter_input(INPUT_POST, 'video');
+        $product['description'] = filter_input(INPUT_POST, 'description');
+        
+        return $product;
+    }
+    
+    /**
+     * ACTION HEPLER: processUploadedImage
+     * Get the uploaded image and move to the img directory
+     * @param string $name name of (HTML)field
+     * @return string Path of the image
+     */
+    private function processUploadedImage($name)
+    {   
+        $usablePath = "";
+        $destinationPath = ROOT . '/public/img/';
+        $tmpPath = $_FILES["$name"]['tmp_name'];
+        
+        if (is_uploaded_file($tmpPath))
+        { 
+            $destinationPath .= $_FILES["$name"]['name'];
+            move_uploaded_file($tmpPath, $destinationPath);
+            $usablePath = ".." . strstr($destinationPath, "/public");
+        }
+        
+        return $usablePath;
     }
 }
