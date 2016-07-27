c80372a5a1c4cf49292eeb247cbad19444cd2ead
eyao06 <eyao06@yahoo.com>
2016-07-27 15:26:56 -0700

    modified text overflow, updated filter, zero results message

--- application/view/listing/index.php
+++ application/view/listing/index.php
@@ -38,49 +38,6 @@
 			border-radius: 0px;
 		}
 
-		.product.list-group-product
-		{
-			float: none;
-			width: 100%;
-			background-color: #fff;
-			margin-bottom: 10px;
-		}
-		.product.list-group-product:nth-of-type(odd):hover,.product.list-group-product:hover
-		{
-			background: #BA55D3;
-		}
-
-		.product.list-group-product .list-group-image
-		{       
-			margin-right: 10px;
-		}
-		.product.list-group-product .thumbnail
-		{
-			margin-bottom: 0px;
-		}
-		.product.list-group-product .caption
-		{
-			padding: 9px 9px 0px 9px;
-		}
-		.product.list-group-product:nth-of-type(odd)
-		{
-			background: #eeeeee;
-		}
-
-		.product.list-group-product:before, .product.list-group-product:after
-		{
-			display: table;
-			content: " ";
-		}
-
-		.product.list-group-product img
-		{
-			float: left;
-		}
-		.product.list-group-product:after
-		{
-			clear: both;
-		}
 		.list-group-product-text
 		{
 			margin: 0 0 11px;
@@ -106,74 +63,80 @@
 			padding-left: 210px;
 			width: 100%;
 		}
+
+		.txt-overflow-hidden {
+		    white-space: nowrap; 
+		    overflow: hidden;
+		    text-overflow: ellipsis; 
+		}
 	</style>
 
 
-	<div class="container-fluid">
-		<!-- Fixed Left Navbar -->
-		<div class="left-navbar-container">
-			<h4>Filter By:</h4>
-			<br>
-			<ul>
-				<!-- Filter By Departments -->
-				<h5>Departments</h5>
-				<li>
-					<a href="#">Link</a>
-				</li>
-				<li>
-					<a href="#">Link</a>
-				</li>
-				<li>
-					<a href="#">Link</a>
-				</li>
-				<li>
-					<a href="#">Link</a>
-				</li>
+<div class="container-fluid">
+	<!-- Fixed Left Navbar -->
+	<div class="left-navbar-container">
+		<h4>Filter By:</h4>
+		<hr>
+		<ul>
+			<!-- Filter By Departments -->
+			<h5>Departments</h5>
+			<li>
+				<a href="#">Link</a>
+			</li>
+			<li>
+				<a href="#">Link</a>
+			</li>
+			<li>
+				<a href="#">Link</a>
+			</li>
+			<li>
+				<a href="#">Link</a>
+			</li>
+			<hr>
+			<!-- Filter By Sellers Avg. Rating -->
+			<h5>Sellers Avg. Rating</h5>
+			<div class="center">
+				<a href="<?php echo URL; ?>">
+					<span>80% & Up</span>
+				</a>
 				<br>
+				<a href="<?php echo URL; ?>">
+					<span>60% & Up</span>
+				</a>
+				<br>
+				<a href="<?php echo URL; ?>">
+					<span>40% & Up</span>
+				</a>
+				<br>
+				<a href="<?php echo URL; ?>">
+					<span>20% & Up</span>
+				</a>
+			</div>
+			<hr>
+			<!-- Filter By # of reviews -->
+			<h5># of Buyers Reviews</h5>
+			<div class="center">
+				<div class="row">
 
-				<!-- Filter By Sellers Avg. Rating -->
-				<h5>Seller Avg. Rating</h5>
-				<div class="center">
-					<div class="row">
-						<div class="col-md-6">
-							<div class="input-group">
-								<small>From:</small>
-								<input type="text" name="minRating" class="form-control input-number" placeholder="Min">
+					<div class="input-group">
+						<div class="btn-group">
+							<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+								<small>Greater Than:</small> <span class="caret"></span>
+							</button>
+							<ul class="dropdown-menu" role="menu">
+								<li><a href="#"><small>Greater Than:</small></a></li>
+								<li><a href="#"><small>Less Than:</small></a></li>
+								<li><a href="#"><small>Equal To:</small></a></li>
+							</ul>
+						</div>
+						<input type="text" name="numreview" class="form-control input-number" placeholder="#">
 
-							</div>
-						</div>
-						<div class="col-md-6">
-							<div class="input-group">
-								<small>To:</small>
-								<input type="text" name="maxRating" class="form-control input-number" placeholder="Max">
-							</div>
-						</div>
 					</div>
 				</div>
-				<br><br>
-
-				<!-- Filter By # of reviews -->
-				<h5># of Reviews:</h5>
-				<div class="center">
-					<div class="row">
-						<div class="col-md-6">
-							<div class="input-group">
-								<small>From:</small>
-								<input type="text" name="minreview" class="form-control input-number" placeholder="Min">
-
-							</div>
-						</div>
-						<div class="col-md-6">
-							<div class="input-group">
-								<small>To:</small>
-								<input type="text" name="maxReview" class="form-control input-number" placeholder="Max">
-							</div>
-						</div>
-					</div>
-				</div>
-			</ul>
-		</div> <!-- end navbar -->
-	</div> <!--end container -->
+			</div>
+		</ul>
+	</div> <!-- end navbar -->
+</div> <!--end container -->
 
 	<div class="container-fluid listing-header">
 		<div class="well">
@@ -203,61 +166,44 @@
 							<option value="5">Avg. Seller Ratings</option>
 						</select>
 					</div>
-
-					<!-- grid/listing view of products -->
-					<div class=" col-md-2">
-						<a href="#" id="list" class="btn btn-default btn-sm">
-							<span class="glyphicon glyphicon-th-list"></span>
-							List
-						</a> 
-					</div>                     
-					<div class=" col-md-2">
-						<a href="#" id="grid" class="btn btn-default btn-sm">
-							<span class="glyphicon glyphicon-th"></span>
-							Grid
-						</a>
-					</div> 
 				</div>
 			</div>
 		</div>
 		<!-- # of results found -->
-		<p><big><strong class="text-danger">#</strong> results found</big></p>
+		<p><big><strong class="text-danger"><?php echo count($products); ?> </strong> results found</big></p>
 	</div>
 	
 	<!-- Product Listing -->
 	<div class="container-fluid listing-body">
-		<div id="products" class="row-fluid list-group">
-			<div id="products" class="row-fluid list-group">
+		<div id="products" class="row-fluid">
+			<ul id="products" class="thumbnails list-unstyled">
 
 				<!-- product here -->
 				<?php foreach ($products as $product) { ?>
-					<div class="product  col-xs-3 col-lg-3">
+					<li class="col-xs-3 col-lg-3">
 						<div class="thumbnail">
 							<a href="<?php echo URL . "product/index/$product->productID"; ?>">
-								<img class="group list-group-image" style="width:250px;height:250px" src="../<?php echo $product->imagePath; ?>" alt="Cannot load image." />
+								<img class="group" style="width:250px height:250px" src="../<?php echo $product->imagePath; ?>" alt="Cannot load image." />
 							</a>
 							<div class="caption">
 
 								<!-- Product Title -->
-								<h4 class="group inner list-group-product-heading">
+								<h4 class="group inner txt-overflow-hidden ">
 									<b><?php echo $product->name; ?></b>
 								</h4>
 
 								<!-- Product price, seller avg. rating, # of reviews --> 
-								<p class="group inner list-group-product-text">
+								<p class="group inner">
 									$ <?php echo $product->price; ?>
 									<br> 
-									<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
-									<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
-									<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
-									<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
-									<span class="glyphicon glyphicon-star" aria-hidden="true"></span>
+									<split><span class="glyphicon glyphicon-thumbs-up"></span> : %%</split>
+									<split><span class="glyphicon glyphicon-thumbs-down"></span>: %%</split>
 									<br>
 									<sup>(# of Reviews)</sup>                       
 								</p>
 
 								<!-- Add To Cart/ But It Now button -->
-								<div class="row">
+								<div class="row" align="center">
 									<div class="col-xs-12 col-md-6">
 										<a class="btn btn-success" href="#">Add to cart</a>
 									</div>
@@ -265,11 +211,17 @@
 										<a class="btn btn-success" href="#">Buy Now</a>
 									</div>
 								</div> <!-- end row -->
-							</div>
+								</div>
 						</div>
-					</div> <!-- end of product -->
+					</li> <!-- end of product -->
 				<?php } ?> <!-- end php -->
-			</div>
+
+				<!-- if no results found -->
+				<?php if (count($products) == 0) { 
+					echo "<h2><em>No results were found.</em></h2>"; 
+				} ?>
+				
+			</ul>
 		</div>
 	</div>
 </body>
