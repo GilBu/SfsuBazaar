4fd80289db9e920752a1c8224178b5a886c71a30
Karen Hanko <khanko@mail.sfsu.edu>
2016-08-01 23:08:14 +0000

    Updated database.php adding functions to order products by price

--- application/model/database.php
+++ application/model/database.php
@@ -4,35 +4,35 @@
  * Singleton class Database
  */
 
-class Database 
-{   
+class Database
+{
     private $db = null;
     private static $instance = null;
 
-    private function __construct() 
+    private function __construct()
     {
         $this->db = $this->openDatabaseConnection();
     }
-        
+
     private function __clone() {}
-    
+
     public static function getInstance()
-    {   
-        if (!isset(self::$instance)) 
+    {
+        if (!isset(self::$instance))
         {
-            try 
-            {   
+            try
+            {
                 self::$instance = new Database();
-            } 
-            catch (PDOException $e) 
+            }
+            catch (PDOException $e)
             {
                 exit('Database connection could not be established.');
             }
         }
-        
+
         return self::$instance;
     }
-    
+
     /**
      * Open the database connection with the credentials from application/config/config.php
      */
@@ -46,114 +46,114 @@ class Database
 
         // generate a database connection, using the PDO connector
         // @see http://net.tutsplus.com/tutorials/php/why-you-should-be-using-phps-pdo-for-database-access/
-        return new PDO(DB_TYPE . ':host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=' . DB_CHARSET, DB_USER, DB_PASS, $options);        
-    } 
-    
-    
+        return new PDO(DB_TYPE . ':host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=' . DB_CHARSET, DB_USER, DB_PASS, $options);
+    }
+
+
     /************************ User queries ************************************/
-    
+
     public function addUser($user)
     {
-	$sql =  "INSERT INTO user " .
-		"(firstName, lastName, password, email, imagePath)" .
-		"VALUES " .
-		"(:firstName, :lastName, :password, :email, :imagePath)";
-	$query = $this->db->prepare($sql);
-	$param = array(	':firstName' 	=> $user->$firstName,
-			':lastName' 	=> $user->$lastName,
-			':password'	=> $user->$password,
-			':email' 	=> $user->$email,
-			':imagePath'	=> $user->$imagePath	);
+        $sql =  "INSERT INTO user " .
+            "(firstName, lastName, password, email, imagePath)" .
+            "VALUES " .
+            "(:firstName, :lastName, :password, :email, :imagePath)";
+        $query = $this->db->prepare($sql);
+        $param = array(	':firstName' 	=> $user->$firstName,
+            ':lastName' 	=> $user->$lastName,
+            ':password'	=> $user->$password,
+            ':email' 	=> $user->$email,
+            ':imagePath'	=> $user->$imagePath	);
 
-	$query->execute($param);
+        $query->execute($param);
     }
 
     public function getUserByID($userID)
     {
-	$sql =  "SELECT * FROM user WHERE " .
-		"userID = :userID LIMIT 1";
-	$query = $this->db->prepare($sql);
-	$params = array(':userID'	=> $userID);
-	$query->execute($params);
+        $sql =  "SELECT * FROM user WHERE " .
+            "userID = :userID LIMIT 1";
+        $query = $this->db->prepare($sql);
+        $params = array(':userID'	=> $userID);
+        $query->execute($params);
 
-	return $query->fetch();
+        return $query->fetch();
     }
 
     public function updateUser(	$firstName, $lastName, $password,
-				$email, $imagePath, $userID	)
+                                   $email, $imagePath, $userID	)
     {
-	//TODO: This is a temporary model of how this could work.
-	//      Needs a permanent solution.
+        //TODO: This is a temporary model of how this could work.
+        //      Needs a permanent solution.
 
-	$sql = 	"UPDATE user SET " .
-		"firstName = :firstName, lastName = :lastName," .
-		"password = :password, email = :email," .
-		"imagePath = :imagePath" .
-		"WHERE userID = :userID";
-	$query = $this->db->prepare($sql);
-	$params = array(	':firstName'	=> $firstName,
-				':lastName'	=> $lastName,
-				':password'	=> $password,
-				':email'	=> $email,
-				':imagePath'	=> $imagePath,
-				':userID'	=> $userID	);
+        $sql = 	"UPDATE user SET " .
+            "firstName = :firstName, lastName = :lastName," .
+            "password = :password, email = :email," .
+            "imagePath = :imagePath" .
+            "WHERE userID = :userID";
+        $query = $this->db->prepare($sql);
+        $params = array(	':firstName'	=> $firstName,
+            ':lastName'	=> $lastName,
+            ':password'	=> $password,
+            ':email'	=> $email,
+            ':imagePath'	=> $imagePath,
+            ':userID'	=> $userID	);
 
-	$query->execute($params);
+        $query->execute($params);
     }
 
     public function deleteUserByID($userID)
     {
-	$sql =  "DELETE FROM user WHERE userID = :userID";
-	$query = $this->db->prepare($sql);
-	$params = array(':userID' => $userID);
+        $sql =  "DELETE FROM user WHERE userID = :userID";
+        $query = $this->db->prepare($sql);
+        $params = array(':userID' => $userID);
 
-	$query->execute($params);
+        $query->execute($params);
     }
 
     /************************ Meetup queries **********************************/
     /**
-	Gets all data of the meetup table from he database
-	@return Array of meetup objects
+    Gets all data of the meetup table from he database
+    @return Array of meetup objects
      */
 
-	public function addMeetup($meetup)
-	{
-		$sql =  "INSERT INTO meetup " .
-			"(status, sellerID, " .
-			"dateOfMeetup, timeOfMeetup, locationOfMeetup)" .
-			"VALUES " .
-			"(:status, :sellerID, " .
-			":dateOfMeetup, :timeOfMeetup, :locationOfMeetup)";
-		$query = $this->db->prepare($sql);
-		$param = array(	':status' 	   => $meetup->status,
-				':sellerID'	   => $meetup->sellerID,
-				':dateOfMeetup'	   => $meetup->dateOfMeetup,
-				':timeOfMeetup'	   => $meetup->timeOfMeetup,
-				':locationOfMeetup' => $meetup->locationOfMeetup	);
-		$query->execute($param);
-	}
+    public function addMeetup($meetup)
+    {
+        $sql =  "INSERT INTO meetup " .
+            "(status, sellerID, " .
+            "dateOfMeetup, timeOfMeetup, locationOfMeetup)" .
+            "VALUES " .
+            "(:status, :sellerID, " .
+            ":dateOfMeetup, :timeOfMeetup, :locationOfMeetup)";
+        $query = $this->db->prepare($sql);
+        $param = array(	':status' 	   => $meetup->status,
+            ':sellerID'	   => $meetup->sellerID,
+            ':dateOfMeetup'	   => $meetup->dateOfMeetup,
+            ':timeOfMeetup'	   => $meetup->timeOfMeetup,
+            ':locationOfMeetup' => $meetup->locationOfMeetup	);
+        $query->execute($param);
+    }
 
-	public function getMeetupByID($meetID)
-	{
-		$sql = "SELECT * FROM meetup WHERE meetID = :meetID LIMIT 1";
-		$query = $this->db->prepare($sql);
-		$param = array(':meetID' => $meetID);
-		$query->execute($param);
+    public function getMeetupByID($meetID)
+    {
+        $sql = "SELECT * FROM meetup WHERE meetID = :meetID LIMIT 1";
+        $query = $this->db->prepare($sql);
+        $param = array(':meetID' => $meetID);
+        $query->execute($param);
 
-		return $query->fetch();
-	}
+        return $query->fetch();
+    }
 
-	public function deleteMeetupByID($meetID)
-	{
-		$sql = "DELETE * FROM meetup WHERE meetID = :meetID";
-		$query = $this->db->prepare($sql);
-		$param = array(':meetID' => $meetID);
-		$query->execute($param);
-	}
+    public function deleteMeetupByID($meetID)
+    {
+        $sql = "DELETE * FROM meetup WHERE meetID = :meetID";
+        $query = $this->db->prepare($sql);
+        $param = array(':meetID' => $meetID);
+        $query->execute($param);
+    }
 
     /************************ Product queries *********************************/
-    
-     /**
+
+    /**
      * Gets all data of the product table from the database
      * @return Array of product objects
      */
@@ -162,11 +162,11 @@ class Database
         $sql = "SELECT * FROM product";
         $query = $this->db->prepare($sql);
         $query->execute();
-        
+
         return $query->fetchAll();
     }
-    
-    /**        
+
+    /**
      * Get all products from the database with a similiar keyword in name
      *
      */
@@ -178,7 +178,35 @@ class Database
 
         return $query->fetchAll();
     }
-    
+
+    /**
+     * Returns products ordered by the highest price
+     * @param $keyword is the search term
+     * @return array of products
+     */
+    public function getProductsByName_PriceHighToLow($keyword)
+    {
+        $sql = "SELECT * FROM product WHERE name LIKE '%$keyword%' ORDER BY price";
+        $query = $this->db->prepare($sql);
+        $query->execute();
+
+        return $query->fetchAll();
+    }
+
+    /**
+     * Returns products ordered by lowest price
+     * @param $keyword is the search term
+     * @return array of products
+     */
+    public function getProductByName_PriceLowToHigh($keyword)
+    {
+        $sql = "SELECT * FROM product WHERE name LIKE '%$keyword%' ORDER BY price DESC";
+        $query = $this->db->prepare($sql);
+        $query->execute();
+
+        return $query->fetchAll();
+    }
+
     /**
      * With the given tag(department) and keyword(search term), returns a list of products with
      * the given tag and and a name similar the keyword, and then products without the same keyword
@@ -189,7 +217,8 @@ class Database
      */
     public function getProductsWithTags($tag, $keyword)
     {
-        $sql = "Select * from product p where p.tags like '$tag' AND p.name like '%$keyword%'";
+        $sql = "Select * from product p where p.tags like '$tag' AND p.name like '%$keyword%' 
+                order by price";
         $query = $this->db->prepare($sql);
         $query->execute();
 
@@ -199,14 +228,71 @@ class Database
                 where p.name like '%$keyword%' and p.productID not in (
                 	select s.productID
                 	from product s 
-                	where s.tags like '$tag')";
+                	where s.tags like '$tag')
+                order by price";
         $query = $this->db->prepare($sql);
         $query->execute();
         $result += $query->fetchAll();
 
         return $result;
     }
-    
+
+    /**
+     * Gets products with tags, and orders the results by the highest price to the lowest price
+     * @param $tag is the dapartment
+     * @param $keyword is the search term
+     * @return array of products
+     */
+    public function getProductsWithTags_PriceHighToLow($tag, $keyword)
+    {
+        $sql = "Select * from product p where p.tags like '$tag' AND p.name like '%$keyword%'
+                 order by price";
+        $query = $this->db->prepare($sql);
+        $query->execute();
+
+        $result = $query->fetchAll();
+        $sql = "Select * 
+                from product p
+                where p.name like '%$keyword%' and p.productID not in (
+                	select s.productID
+                	from product s 
+                	where s.tags like '$tag')
+                order by price";
+        $query = $this->db->prepare($sql);
+        $query->execute();
+        $result += $query->fetchAll();
+
+        return $result;
+    }
+
+    /**
+     * Gets products with tags, and orders the results by the lowest price to the highest price
+     * @param $tag is the dapartment
+     * @param $keyword is the search term
+     * @return array of products
+     */
+    public function getProductsWithTags_PriceLowToHigh($tag, $keyword)
+    {
+        $sql = "Select * from product p where p.tags like '$tag' AND p.name like '%$keyword%'
+                 order by price";
+        $query = $this->db->prepare($sql);
+        $query->execute();
+
+        $result = $query->fetchAll();
+        $sql = "Select * 
+                from product p
+                where p.name like '%$keyword%' and p.productID not in (
+                	select s.productID
+                	from product s 
+                	where s.tags like '$tag')
+                order by price";
+        $query = $this->db->prepare($sql);
+        $query->execute();
+        $result += $query->fetchAll();
+
+        return $result;
+    }
+
     /**
      * Get the product with the giving id
      * @param int $id
@@ -215,12 +301,12 @@ class Database
     {
         $sql = "SELECT * FROM product WHERE productID = :productID LIMIT 1";
         $query = $this->db->prepare($sql);
-        $param = array(':productID' => $productID); 
+        $param = array(':productID' => $productID);
         $query->execute($param);
-        
+
         return $query->fetch();
     }
-    
+
     /**
      * @return a random product object
      */
@@ -232,7 +318,7 @@ class Database
 
         return $query->fetch();
     }
-    
+
     /**
      * @return a list of random products
      */
@@ -244,8 +330,8 @@ class Database
 
         return $query->fetch();
     }
-    
-     /**
+
+    /**
      * Delete the product with the giving id
      * @param int $id
      */
@@ -253,52 +339,65 @@ class Database
     {
         $sql = "DELETE FROM product WHERE productID = :productID";
         $query = $this->db->prepare($sql);
-        $param = array(':productID' => $productID); 
+        $param = array(':productID' => $productID);
         $query->execute($param);
     }
 
-    
+
     /**
      * Add a product to the db
      * @param Product with all data about the product
      */
-    public function addProduct($product) 
+    public function addProduct($product)
     {
         $sql = "INSERT INTO product " .
-               "(name, sellerID, price, quantity, quality, imagePath, "
-                . "videoUrl, description, tags, isService)" .
-               "VALUES ".
-               "(:name, :sellerID, :price, :quantity, :quality, :imagePath, "
-                . ":videoUrl, :description, :tags, :isService)";
-               
+            "(name, sellerID, price, quantity, quality, imagePath, "
+            . "videoUrl, description, tags, isService)" .
+            "VALUES ".
+            "(:name, :sellerID, :price, :quantity, :quality, :imagePath, "
+            . ":videoUrl, :description, :tags, :isService)";
+
         $query = $this->db->prepare($sql);
-        $param = array( ':name' => $product->name, 
-                        ':sellerID' => $product->sellerID,
-                        ':price' => $product->price, 
-                        ':quantity' => $product->quantity, 
-                        ':quality' => $product->quality, 
-                        ':imagePath' => $product->imagePath,
-                        ':videoUrl' => $product->videoUrl,
-                        ':description' => $product->description, 
-                        ':tags' => $product->tags, 
-                        ':isService' => $product->isService);
-        
+        $param = array( ':name' => $product->name,
+            ':sellerID' => $product->sellerID,
+            ':price' => $product->price,
+            ':quantity' => $product->quantity,
+            ':quality' => $product->quality,
+            ':imagePath' => $product->imagePath,
+            ':videoUrl' => $product->videoUrl,
+            ':description' => $product->description,
+            ':tags' => $product->tags,
+            ':isService' => $product->isService);
+
         $query->execute($param);
     }
-    
-         /************************ Reveiw queries ************************************/
+
+    /**
+     * Gets products ordered by the most recently added to the oldest.
+     * @return array of products
+     */
+    public function getProductByMostRecent()
+    {
+        $sql = "SELECT * FROM product ORDER BY createdAt";
+        $query = $this->db->prepare($sql);
+        $query->execute();
+
+        return $query->fetchAll();
+    }
+
+    /************************ Reveiw queries ************************************/
 
     public function addReview($reviewerID, $userID, $raiting, $comment)
     {
         $sql =  "INSERT INTO review " .
-                "(reviewerID, userID, raiting, comment)" .
-                "VALUES " .
-                "(:reviewerID, :userID, :raiting, :comment)";
+            "(reviewerID, userID, raiting, comment)" .
+            "VALUES " .
+            "(:reviewerID, :userID, :raiting, :comment)";
         $query = $this->db->prepare($sql);
         $param = array( ':reviewerID'   => $reviewerID,
-                        ':userID'       => $userID,
-                        ':raiting'      => $raiting,
-                        ':comment'      => $comment);
+            ':userID'       => $userID,
+            ':raiting'      => $raiting,
+            ':comment'      => $comment);
 
         $query->execute($param);
     }
@@ -306,40 +405,40 @@ class Database
     public function getReviewByID($reviewID)
     {
         $sql =  "SELECT " .
-                "reviewID" .
-                "FROM review WHERE " .
-                "reviewID = :reviewID LIMIT 1";
+            "reviewID" .
+            "FROM review WHERE " .
+            "reviewID = :reviewID LIMIT 1";
         $query = $this->db->prepare($sql);
         $params = array(':reviewID'     => $reviewID);
 
         $query->execute($params);
     }
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
+
+
+
+
 
     /**
      * Get all products from db with the giving name
      * @param string $name
      * @return Array Contain products with the same name in db
      */
-//    public function getByName($name) 
+//    public function getByName($name)
 //    {
 //        $sql = "SELECT * FROM products WHERE name = '$name'";
 //        $query = $this->db->prepare($sql);
 //        $query->execute();
-//        
+//
 //        return $query->fetchAll();
 //    }
-    
+
     /**
      * Change a Associative Array to the PDO param format
      * i.e. change array['key']['value'] to array[':key']['value']
@@ -349,12 +448,13 @@ class Database
 //    public function arrayToPDOParam($array)
 //    {
 //        $PDOParam = array();
-//        
-//        foreach ($array as $key => $value) 
+//
+//        foreach ($array as $key => $value)
 //        {
 //            $PDOParam[":$key"] = $value;
 //        }
-//        
+//
 //        return $PDOParam;
 //    }
 }
+
