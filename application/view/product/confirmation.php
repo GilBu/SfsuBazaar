6aa3382d8be4172fb70af4cdb77982ce77c0a143
eyao06 <eyao06@yahoo.com>
2016-08-01 23:10:29 -0700

    created confirmation page

---
+++ application/view/product/confirmation.php
@@ -0,0 +1,88 @@
+<style>
+    .table>tbody>tr>td, .table>tfoot>tr>td{
+        vertical-align: middle;
+    }
+
+    @media screen and (max-width: 600px) {
+        table#product tbody td .form-control{
+            width:20%;
+            display: inline !important;
+        }
+        .actions .btn{
+            width:36%;
+            margin:1.5em 0;
+        }
+
+        table#product thead { 
+            display: none; 
+        }
+        table#product tbody td { 
+            display: block; padding: .6rem; min-width:320px;
+        }
+        table#product tbody tr td:first-child { 
+            background: #333; color: #fff; 
+        }
+        table#product tbody td:before {
+            content: attr(data-th); font-weight: bold;
+            display: inline-block; width: 8rem;
+        }
+
+        table#product tfoot td{
+            display:block; 
+        }
+        table#product tfoot td .btn{
+            display:block;
+        }
+
+    }
+</style>
+
+<div class="container">
+    <h1>Confirm Purchase</h1>
+    <table id="product" class="table table-hover table-condensed">
+        <thead>
+            <tr>
+                <th style="width:40%">Product</th>
+                <th style="width:30%">Seller</th>
+                <th style="width:30%">Price</th>
+            </tr>
+        </thead>
+
+        <!-- Place Products Here -->
+        <tbody>
+            <tr>
+                <!-- Product -->
+                <td data-th="Product">
+
+                    <!-- Product img and title -->
+                    <div class="row">
+                        <div class="col-sm-4 hidden-xs">
+                            <img src="../../<?php echo $product->imagePath; ?>"/>
+                        </div>
+                        <div class="col-sm-6">
+                            <h4><?php echo $product->name; ?></h4>
+                        </div>
+                    </div>
+                    
+                </td>
+
+                <!-- Seller Username -->
+                <td data-th="Seller">????????????????</td>
+
+                <!-- Product Price -->
+                <td data-th="Price"> $ <?php echo $product->price; ?></td>
+
+            </tr>
+        </tbody>
+
+        <!-- Cancel/Confirm Purchase -->
+        <tfoot>
+            <!-- Cancel/Confirm Purchase Button -->
+            <tr>
+                <td><a href="#" class="btn btn-warning"><i class="fa fa-angle-left"></i> Cancel</a></td>
+                <td colspan="1" class="hidden-xs"></td>
+                <td><a href="#" class="btn btn-success btn-block"> Confirm<i class="fa fa-angle-right"></i></a></td>
+            </tr>
+        </tfoot>
+    </table>
+</div>
