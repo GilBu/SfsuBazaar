52f0c46451ada5d9f6da39fcf39d2dcc87e48eb7
Guan Lin <glin@mail.sfsu.edu>
2016-08-05 15:04:26 -0700

    changed error page text

--- application/view/error/index.php
+++ application/view/error/index.php
@@ -1,3 +1,3 @@
 <div class="container">
-    <p>This is the Error-page. Will be shown when a page (= controller / method) does not exist.</p>
+    <p>Opps, Are you going somewhere you are not belong?</p>
 </div>
