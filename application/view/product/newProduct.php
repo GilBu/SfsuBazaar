816f450a069366dac708308a11e14c0627fe39e8
eyao06 <eyao06@yahoo.com>
2016-08-05 01:32:40 -0700

    modified min input value for pricing to 0.01

--- application/view/product/newProduct.php
+++ application/view/product/newProduct.php
@@ -45,7 +45,7 @@
 				<div class="form-group">
 					<label class="control-label col-sm-2" for="name">Price:</label>
 					<div class="col-sm-10">
-						<input type="number" step="0.01" min="1" class="form-control" name="price" value="" required id="price" placeholder="Enter Product Price"/>
+						<input type="number" step="0.01" min="0.01" class="form-control" name="price" value="" required id="price" placeholder="Enter Product Price"/>
 					</div>
 				</div>
 
@@ -148,7 +148,7 @@
 				<div class="form-group">
 					<label class="control-label col-sm-2" for="name">Price Per Hour: </label>
 					<div class="col-sm-10">
-						<input type="number" step="0.01" min="1" class="form-control" name="price" value="" required id="price" placeholder="Enter Product Price"/>
+						<input type="number" step="0.01" min="0.01" class="form-control" name="price" value="" required id="price" placeholder="Enter Product Price"/>
 					</div>
 				</div>
 
